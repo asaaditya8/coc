9fc2312304217363262cb45ab037d6132592fb88
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2020-05-14 15:48:54 +0530

    Initialized project.

---
+++ exp.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: notebooks//ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.1
+#   kernelspec:
+#     display_name: fastai
+#     language: python
+#     name: fastai
+# ---
+
+# %%
