b04e632c5d614e0a439d201e510dee703286ded6
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2020-05-14 19:36:48 +0530

    Vectorized strategies.csv, Added upgrades.csv

--- exp.py
+++ exp.py
@@ -14,3 +14,35 @@
 # ---
 
 # %%
+import pandas as pd
+
+# %%
+FILENAME = '../strategies.csv'
+
+# %%
+df = pd.read_csv(FILENAME)
+
+# %%
+df.head()
+
+
+# %%
+def foo(x):
+    if x is troop_name:
+        return 1
+    else:
+        return 0
+
+new_cols = [df.columns[0]]
+df_bool = df.copy()
+for i, col in enumerate(df.columns[1:]):
+    troop_name = [x for x in df.loc[:, col].unique() if type(x) is str][0]
+    df_bool.loc[:, col] = df.loc[:, col].map(lambda x: x is troop_name)
+    new_cols.append(troop_name)
+
+df_bool.columns = pd.Index(new_cols)
+
+# %%
+df_bool.head()
+
+# %%
